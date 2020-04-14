5ee466105de71045c2073cb6388f8e1ff04ebac1
LOMMELEN <els.lommelen@inbo.be>
2020-04-14 15:27:10 +0200

    replace 'Descriptor index' error with informative error

--- R/odbc_dbGetQuery.R
+++ R/odbc_dbGetQuery.R
@@ -12,7 +12,16 @@ setMethod(
     rs <- dbSendQuery(conn, statement, params = params, ...)
     on.exit(dbClearResult(rs))
 
-    df <- dbFetch(rs, n = n, ...),
+    tryCatch(
+      df <- dbFetch(rs, n = n, ...),
+      error = function(e) {
+        if (grepl("Descriptor", e) & grepl("index", tolower(e))) {
+          stop("Query problem: put columns with long data (text, image, varchar(max),... at the end of the select statement")
+        } else {
+          stop(e)
+        }
+      }
+    )
 
     if (!dbHasCompleted(rs)) {
       warning("Pending rows", call. = FALSE)
