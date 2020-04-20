ced7bf9015950d1fa4ea24aa7640561135008e57
Els Lommelen <els.lommelen@inbo.be>
2020-04-20 13:37:39 +0200

    add remark on (n)varcharmax to error for Linux users

--- R/odbc_dbGetQuery.R
+++ R/odbc_dbGetQuery.R
@@ -19,6 +19,9 @@ setMethod(
         if (grepl("Descriptor", e) & grepl("index", tolower(e))) {
           info <- dbColumnInfo(rs)
           columns <- paste0(info[info$type %in% c("-1", "-2", "-3", "-4", "-10", "-151", "-152"), "name"], collapse = "', '")
+          if (.Platform$OS.type == "unix") {
+            columns <- paste0(columns, "' and fields of the datatypes 'nvarchar(max)' and 'varchar(max)")
+          }
           stop(paste0("Query problem: put field(s) '", columns, "' at the end of the select statement."))
         } else {
           stop(e)
