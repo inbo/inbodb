7d1e0e8cd254c15eb565f097866bb7798b988d3e
Els Lommelen <els.lommelen@inbo.be>
2020-04-22 13:52:30 +0200

    move method dbDisconnect to separate file

--- R/connect_inbo_dbase.R
+++ R/connect_inbo_dbase.R
@@ -63,41 +63,6 @@ connect_inbo_dbase <- function(database_name) {
     return(conn)
 }
 
-on_connection_closed <- function(connection) {
-    # make sure we have an observer
-    observer <- getOption("connectionObserver")
-    if (is.null(observer))
-        return(invisible(NULL))
-
-    # provide information no DWH or database
-    if (grepl("08", connection@info$servername)) {
-        type <- "INBO DWH Server"
-    } else if (grepl("07", connection@info$servername)) {
-        type <- "INBO PRD Server"
-    }
-
-    observer$connectionClosed(type, connection@info$dbname)
-}
-
-#' Overwrite the odbc function
-#'
-#' @inheritParams DBI::dbDisconnect
-#'
-#' @importFrom odbc dbIsValid
-#' @importFrom utils getFromNamespace
-#' @export
-setMethod(
-    "dbDisconnect", "OdbcConnection",
-    function(conn, ...) {
-        if (!dbIsValid(conn)) {
-            warning("Connection already closed.", call. = FALSE)
-        }
-
-        on_connection_closed(conn)
-        conn_release = getFromNamespace("connection_release", "odbc")
-        conn_release(conn@ptr)
-        invisible(TRUE)
-    })
 
 #' Rstudio Viewer integration
 #'
