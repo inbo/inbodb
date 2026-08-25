381246f176737ad71289774c0d4bdfc8a745d698
ElsLommelen <els.lommelen@inbo.be>
2026-08-25 13:55:53 +0200

    use tcp protocol for connection to make sure timeout default for DBI (10 s) is respected

--- R/connect_inbo_dbase.R
+++ R/connect_inbo_dbase.R
@@ -49,11 +49,11 @@ connect_inbo_dbase <- function(database_name, autoconvert_utf8 = deprecated()) {
   # datawarehouse databases (sql08) start with an M, S or W; most
   # transactional (sql07) with a D (by agreement with dba's)
   if (any(startsWith(database_name, c("M", "S", "W")))) {
-    server_new <- "inbo-sql10-prd.inbo.be"
+    server_new <- "tcp:inbo-sql10-prd.inbo.be"
     server_old <- "inbo-sql08-prd.inbo.be"  # DWH server
     type <- "INBO DWH Server"
   } else {
-    server_new <- "inbo-sql09-prd.inbo.be"
+    server_new <- "tcp:inbo-sql09-prd.inbo.be"
     server_old <- "inbo-sql07-prd.inbo.be"  # SQL transactional server
     type <- "INBO PRD Server"
   }
